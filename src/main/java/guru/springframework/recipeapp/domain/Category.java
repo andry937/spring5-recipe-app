ea08c6981c0eb54094b1666c9c31729513b21564
Andry Andrianambinina <andry.andrianambinina@proximitybbdo.fr>
2021-08-10 11:50:07 +0400

    add getter and setter

--- src/main/java/guru/springframework/recipeapp/domain/Category.java
+++ src/main/java/guru/springframework/recipeapp/domain/Category.java
@@ -10,4 +10,20 @@ public class Category extends BaseDomain{
 
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
