ee2daae5fd2d5c913093336f13aaf88d6cbd0cd6
Andry Andrianambinina <andry.andrianambinina@proximitybbdo.fr>
2021-08-10 12:47:22 +0400

    add find by description

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -1,13 +1,30 @@
 package guru.springframework.recipeapp.controllers;
 
+import guru.springframework.recipeapp.domain.Category;
+import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import guru.springframework.recipeapp.repository.CategoryRepository;
+import guru.springframework.recipeapp.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"/", "", "/index"})
     public String getIndexPage() {
+        Optional<Category> category = categoryRepository.findByDescription("Fast Food");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println("Category id is :"+category.get().getId());
+        System.out.println("Unit of measure id is :"+unitOfMeasure.get().getId());
         return "index";
     }
 }
