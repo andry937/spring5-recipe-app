1ab30da1d652ef615d93d5478d61320e7f7dbf14
Andry Andrianambinina <andry.andrianambinina@proximitybbdo.fr>
2021-08-17 09:50:10 +0400

    integration test

---
+++ src/test/java/guru/springframework/recipeapp/repository/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,36 @@
+package guru.springframework.recipeapp.repository;
+
+import guru.springframework.recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryTestIT {
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescriptionTeaspoon() {
+        Optional<UnitOfMeasure> value = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", value.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> value = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", value.get().getDescription());
+    }
+}
